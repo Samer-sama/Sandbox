48280dd599149653c9944b163f4e4e33dcca76a4
samkha <samer.kharabish@ingenics-digital.com>
2023-06-23 23:39:06 +0200

    Deleted Solution

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -1,10 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # TODO
-        if n < 2:
-            return 1
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        pass
 
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.climbStairs(   )) # Missing inout 
+    print(solution.climbStairs(  )) # Missing input 
